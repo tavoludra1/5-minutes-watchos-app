288cd2fe363d19e6f4c6fb7dc222b357b452b7d0
tavoludra1 <tavoludra@hotmail.com>
2025-07-21 17:14:50 -0500

    feat(engine): Crea la lógica y estructura de TimerEngine

---
+++ FiveMinutes/FiveMinutes Watch App/TimerEngine.swift
@@ -0,0 +1,56 @@
+//
+//  TimerEngine.swift
+//  FiveMinutes Watch App
+//
+//  Created by GAPT on 21/07/25.
+//
+
+import Foundation
+import Combine // @Published y temporizador
+
+class TimerEngine: ObservableObject {
+    
+    // @Published cualquier cambio en esta variable sera notificada a la UI
+    @Published var timeLeftString: String = "00:05" // tiempo restante
+    
+    private var timer: AnyCancellable?
+    
+    // pruebas rapidas 5 seg
+    // 300 segundos son 5 minutos
+    private let totalSeconds: Int = 5
+    private var secondsLeft: Int = 5
+    
+    // Iniciar la cuenta regresiva
+    func start() {
+        // Reiniciar los valores
+        secondsLeft = totalSeconds
+        updateTimeLeftString()
+        
+        // configurar el encendido del temporizador
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                self.secondsLeft -= 1
+                self.updateTimeLeftString()
+                
+                if self.secondsLeft <= 0 {
+                    print("¡Temporizador Finalizado!")
+                    // ojo este lugar es para el haptic
+                    self.stop()
+                }
+            }
+    }
+    
+    // Detener la cuenta regresiva
+    func stop() {
+        timer?.cancel()
+        print("Temporizador Detenido 🛑")
+    }
+    
+    // funcion para formatear el tiempo -> minutos:segundos
+    private func updateTimeLeftString() {
+        let minutes = secondsLeft / 60
+        let seconds = secondsLeft % 60
+        self.timeLeftString = String(format: "%02d:%02d", minutes, seconds)
+    }
+}
