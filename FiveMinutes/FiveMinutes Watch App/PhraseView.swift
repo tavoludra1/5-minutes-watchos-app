8691fbd789cc9ae2414c2501bb76b55aeea6ca12
tavoludra1 <tavoludra@hotmail.com>
2025-07-21 19:48:58 -0500

    feat(nav): Añade pausa de 2 segundos en PhraseView

--- FiveMinutes/FiveMinutes Watch App/PhraseView.swift
+++ FiveMinutes/FiveMinutes Watch App/PhraseView.swift
@@ -11,12 +11,30 @@ struct PhraseView: View {
     // Al crear la vista -> frase aleatoria
     let phrase: String = MotivationalPhrases.getRandom()
     
+    // Estado para controlar la navegacion
+    @State private var navigateToTimerView: Bool = false
+    
     
     var body: some View {
-        Text(phrase)
-            .font(.title3)
-            .multilineTextAlignment(.center)
-            .padding()
+        VStack {
+            Text(phrase)
+                .font(.title3)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            }
+        // modificador vista principal
+        // navega cuando cambie a true
+        .navigationDestination(isPresented: $navigateToTimerView) {
+            TimerView()
+        }
+        // logica para el tiempo de pausa
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.navigateToTimerView = true
+            }
+        }
+
     }
 }
 
